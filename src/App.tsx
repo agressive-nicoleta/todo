ac8aef3d8e9dea6657635716e0d17e6ceeb0efed
npetre <npetre@ttu.ee>
2022-08-09 20:38:05 +0300

    New package instaleld

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("Do News App"), new Todo("Do smth")];
 
+  const addTodoHandler = (text: String) => {};
+
   return (
     <div>
       <h1>ToDo App</h1>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,1 +1,27 @@
-export {};
+import classes from "./NewTodo.module.css";
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputText = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputText.current!.value;
+    if (enteredText?.trim().length === 0) {
+      //throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <input type="text" id="text" placeholder="Write here..." />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
